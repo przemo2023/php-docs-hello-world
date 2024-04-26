3f6ba337447c049570e12150bb797f14746fd7c1
przemo2023 <148747208+przemo2023@users.noreply.github.com>
2024-04-26 16:59:48 +0200

    Added tags and styles

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<html>
+<head>
+    <title>Title</title>
+    <style type="text/css">
+    .greetings {
+        color: blue;
+        font-weight: bold;
+    }
+    </style>
+</head>
+<body>
+
+<div class="greetings">
 <?php 
     echo "Pozdrowienia dla Kasi i Bartosza!"; 
 ?>
+<div>
+
+</body>
+</html>
